992d6ef477768c20fc1847b72c57b7725138ed18
Eduardo-RFarias <eduardo.rfarias@outlook.com>
2025-07-27 22:01:05 -0300

    Remove FilesController from SharedModule and delete its implementation to streamline file handling in the application.

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './services/upload/upload.service';
-import { FilesController } from './files/files.controller';
 
 @Module({
   providers: [UploadService],
   exports: [UploadService],
-  controllers: [FilesController],
 })
 export class SharedModule {}
